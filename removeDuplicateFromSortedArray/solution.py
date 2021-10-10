746a0277d9b85b7fe5313b18d29123a7ec1fef28
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-10-10 19:08:53 +0100

    changes added

--- removeDuplicateFromSortedArray/solution.py
+++ removeDuplicateFromSortedArray/solution.py
@@ -1,9 +1,9 @@
 class Solution(object):
-    def removeDuplicates(self, nums):
+    def removeDuplicates(self, nums: List[int]) -> int:
         l = 1
 
         for r in range(1, len(nums)):
             if nums[r] != nums[r - 1]:
-                nums[l] =nums[r]
+                nums[l] = nums[r]
                 l += 1
         return l
