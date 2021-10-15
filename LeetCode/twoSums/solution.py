9ffc62a2e6eb4f73076424cb7faae7bb44e2abe2
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-10-15 16:54:01 +0100

    two sums

--- LeetCode/twoSums/solution.py
+++ LeetCode/twoSums/solution.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def twoSUms(self, nums: List[int], target: int) -> List[int]:
+    def twoSums(self, nums: List[int], target: int) -> List[int]:
         for num in range(len(nums)):
             for n in range(num + 1, len(nums)):
                 if nums[num] + nums[n] == target:
