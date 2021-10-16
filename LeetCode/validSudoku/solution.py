05e22b97ca0593bb7fb5bd0f95a8723259ffadab
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-10-16 22:39:37 +0100

    valid sudoku

--- LeetCode/validSudoku/solution.py
+++ LeetCode/validSudoku/solution.py
@@ -14,4 +14,9 @@ class Soluton:
                     continue
                 if (board[r][c] in rows[r] or
                     board[r][c] in cols[c] or
-                    board[r][c] in squares[r // 3, c // 3])
+                    board[r][c] in squares[r // 3, c // 3]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True
