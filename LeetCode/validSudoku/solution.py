daa75565b6e6a51dd40049fe7719c62247546a94
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-10-16 22:35:24 +0100

    valid sudoku

---
+++ LeetCode/validSudoku/solution.py
@@ -0,0 +1,17 @@
+import collections
+from typing import Collection, List
+
+
+class Soluton:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == "":
+                    continue
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[r // 3, c // 3])
