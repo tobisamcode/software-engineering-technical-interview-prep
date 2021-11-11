a9611dc3d80ef93e7baa0bb7ad19a59d6af574cb
tobisamcodee <samueloluwatobiloba48@gmail.com>
2021-11-11 04:10:57 +0100

    added

--- LeetCode/Strings/reverseString/solution.py
+++ LeetCode/Strings/reverseString/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         '''
-        Do not return anything, modify s in-plane instead
+        Do not return anything, modify s in-plane instead of
         '''
         left = 0
         right = len(s) -1
